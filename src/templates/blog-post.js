2d0e5c9c5c69147215185859124a6084314dc63c
Emmanuel Gautier <git@emmanuelgautier.fr>
2020-11-07 15:27:29 +0100

    add blog post related notebooks

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -22,6 +22,7 @@ const BlogPost = ({ data: { post, category, author, logo, site: { siteMetadata: 
   const publishedDate = post.frontmatter.date
   const hashtags = tags.map(tag => `${tag.split(' ').join('')}`)
   const sources = post.frontmatter.sources
+  const notebooks = post.frontmatter.notebooks
   const readingTime = Math.ceil(+post.fields.readingTime.minutes)
 
   return (
@@ -176,6 +177,20 @@ const BlogPost = ({ data: { post, category, author, logo, site: { siteMetadata: 
               ))}
             </div>
           ) : null}
+
+          {notebooks && notebooks.length ? (
+            <div className="my-4 md:my-8">
+              <h2 className="text-lg text-gray-900 mb-2">Notebooks</h2>
+
+              <hr className="my-4" />
+
+              {notebooks.map(notebook => (
+                <OutboundLink href={notebook.link} target="_blank" rel="noreferrer">
+                  {notebook.title}
+                </OutboundLink>
+              ))}
+            </div>
+          ) : null}
         </div>
       </article>
     </Layout>
@@ -217,6 +232,10 @@ export const pageQuery = graphql`
         sources {
           link
           source
+        }
+        notebooks {
+          link
+          title
         }
       }
     }
