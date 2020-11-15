51ea23d5b0ab817bc1d0d202f61ea48b310c6681
Emmanuel Gautier <git@emmanuelgautier.fr>
2020-11-15 18:14:35 +0100

    fix blog index page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,29 +1,80 @@
+import { graphql } from 'gatsby'
+import { GatsbySeo } from 'gatsby-plugin-next-seo'
 import React from 'react'
 
 import BlogPostCard from '../../components/BlogPostCard'
 import Layout from '../../components/Layout'
 
-import useLatestPosts from '../../queries/latest-posts'
-
-const BlogIndexPage = () => {
-  const posts = useLatestPosts()
+const BlogIndexPage = ({
+  data: {
+    postsAllMarkdownRemark: { edges: postEdges },
+    site: { siteMetadata: { siteUrl, title: siteTitle, description: siteDescription } }
+  },
+}) => {
 
   return (
     <Layout>
-      <div className="content">
-        <h2>Latest Posts</h2>
+      <GatsbySeo
+        title={`${siteTitle} articles`}
+        description={siteDescription}
+        canonical={`${siteUrl}blog/`}
+      />
 
-        {posts.map(({ slug, title, description, featuredimage }) => (
-          <BlogPostCard
-            slug={slug}
-            title={title}
-            description={description}
-            image={featuredimage}
-          />
-        ))}
-      </div>
+      <section className="max-w-3xl mx-auto px-2 sm:px-4 xl:max-w-5xl xl:px-0">
+        <div className="flex mt-16 mb-4 px-4 lg:px-0 items-center justify-between">
+          <h2 className="font-bold text-3xl">Latest Posts</h2>
+        </div>
+
+        <div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postEdges.map(({ node: { fields: { slug }, frontmatter: { title, description, featuredimage } } }) => (
+            <BlogPostCard
+              slug={slug}
+              title={title}
+              description={description}
+              image={featuredimage}
+            />
+          ))}
+        </div>
+      </section>
     </Layout>
   )
 }
 
 export default BlogIndexPage
+
+export const pageQuery = graphql`
+  query BlogIndexPage {
+    postsAllMarkdownRemark: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      skip: 0
+      limit: 10
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            description
+            featuredimage {
+              childImageSharp {
+                fluid(maxHeight: 350) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        siteUrl
+        title
+        description
+      }
+    }
+  }
+`
